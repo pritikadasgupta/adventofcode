0c8ae71d0463e937f7366fd1714c36468e9bd5ad
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2020-12-03 09:01:21 -0500

    day 3 of adventofcode 2020 in python

---
+++ 2020/Day3/toboggan.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import os
+
+# How many passwords are valid according to their policies?
+def tobaggan(ar,right,down):
+	#start at top-left corner
+	idx=0
+	trees=0
+	for x in range(0,len(ar),down):
+		if(ar[x][idx]=='.'):
+			trees=trees+0
+		elif(ar[x][idx]=='#'):
+			trees=trees+1
+		idx = (idx+right)%len(ar[x])
+	return trees
+
+
+with open(os.environ['INPUT_PATH']) as ar_count: 
+	ar = [x for x in ar_count.read().split()]
+
+#part 1
+tobaggan(ar,3,1)
+
+
+#part 2
+tobaggan(ar,1,1)*tobaggan(ar,3,1)*tobaggan(ar,5,1)*tobaggan(ar,7,1)*tobaggan(ar,1,2)
+
+78*262*66*69*30
+
+#2698900776
