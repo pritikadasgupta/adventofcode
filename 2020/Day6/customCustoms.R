e52760e5d7e6ded36dbbfc63075e27ea82cb4268
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2020-12-06 10:21:16 -0500

    day 6 R solutions

---
+++ 2020/Day6/customCustoms.R
@@ -0,0 +1,57 @@
+#!/usr/bin/env Rscript
+
+#Libraries
+library(tidyverse)
+
+#Set working directory and load in data
+data1 <- readLines(file("stdin")) #read in file
+
+#-------------------
+#PART 1
+#-------------------
+
+#Clean up data
+data2 <- paste(data1,collapse = "\n") #concatenate everything into one string
+data3 <- strsplit(data2,"\n\n")[[1]] #find where there are two new lines to separate btwn blanks
+data4 <- gsub("\n"," ",data3) #get rid of the \n
+
+
+# For each group, count the number of questions to which 
+# anyone answered "yes". What is the sum of those counts?
+counts <- vector() #initialize counts vector 
+#For each group, find unique letters
+for(x in data4){
+  x <- strsplit(x,"")[[1]] #split the answers
+  unique_x <- unique(x)
+  x_noblank <- unique_x[!(unique_x %in% c(" "))]
+  counts <- append(counts,length(x_noblank)) #append counts
+}
+
+part1 <- sum(counts)
+print(part1)
+
+#-------------------
+#PART 2
+#-------------------
+counts2 <- vector()
+# questions to which everyone answered "yes"
+
+num <- length(data4) #number of groups
+
+
+
+for(x in data4){
+  #find all unique elements
+  split_x <- strsplit(x,"")[[1]] #split the answers
+  x_noblank <- split_x[!(split_x %in% c(" "))]
+  
+  y <- strsplit(x," ")[[1]] #split the answers for each person in the group
+  num_people <- length(y)
+
+  a <- rle(sort(x_noblank))
+  counts2 <- append(counts2,sum(a$lengths ==num_people))
+}
+
+part2 <- sum(counts2)
+print(part2)
+
