84931abab09e51a2ec989f72c0e460966b4fb92e
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2020-12-07 15:15:11 -0500

    day 7 python soln

--- 2020/Day7/handyHaversacks.py
+++ 2020/Day7/handyHaversacks.py
@@ -4,20 +4,22 @@ import re
 import os
 
 bags = {}
+
 with open(os.environ['INPUT_PATH']) as file:
 	for line in file:
-		bag, sub = line.strip().split(" bags contain ")
+		bag, tobag = line.strip().split(" bags contain ")
 		bags[bag] = []
-		if sub == "no other bags.":continue
-		for subbag in [bag.strip() for bag in sub.split(',')]:
-			num, type = re.match(r'(\d+) (.+) bag.*',subbag).groups()
+		if (tobag == "no other bags."):
+			continue
+		for tobagbag in [bag.strip() for bag in tobag.split(',')]:
+			num, type = re.match(r'(\d+) (.+) bag.*',tobagbag).groups()
 			bags[bag].append((type,int(num)))
 
 def part1(bag):
-	return bag == "shiny gold" or any(part1(sub) for sub, _ in bags[bag])
+	return bag == "shiny gold" or any(part1(tobag) for tobag, _ in bags[bag])
 
 def part2(bag):
-	return 1+ sum(n*part2(sub) for sub,n in bags[bag])
+	return 1+ sum(n*part2(tobag) for tobag,n in bags[bag])
 
 #part 1
 print("Part 1: ",sum(part1(bag) for bag in bags) - 1)
