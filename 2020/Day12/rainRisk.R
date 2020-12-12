ff12e10702f85f5367e840edb931d3c2d11082ea
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2020-12-12 03:51:11 -0500

    day 12 solns in R (works on test input)

---
+++ 2020/Day12/rainRisk.R
@@ -0,0 +1,110 @@
+#!/usr/bin/Rscript
+
+#clear workspace
+rm(list = ls())
+
+#Set working directory and load in data
+# con <- file("stdin", open = "r")
+# data1 <- readLines(con) #read in file
+# close(con)
+
+setwd("~/Documents/adventofcode1/adventofcode/2020/Day12")
+data1 <- readLines("exercise12.input.txt")
+
+#-------------------------------------------------------------------------
+#Clean up data 
+#-------------------------------------------------------------------------
+
+directions <- vector()
+for (datum in data1) {
+  directions <- append(directions,strsplit(datum,"")[[1]][1])
+}
+
+values <- vector()
+for(datum in data1){
+  x <- strsplit(datum,"")[[1]]
+  x <- as.numeric(x[2:length(x)])
+  x <- paste(c(x),collapse="")
+  
+  values <- append(values,as.numeric(x))
+}
+
+df <- as.data.frame(cbind(directions,values))
+
+#-------------------------------------------------------------------------
+#Part 1: Setup
+#-------------------------------------------------------------------------
+
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+#store cardinal directions in a dictionary list
+direction_list <- vector(mode="list", length=4)
+names(direction_list) <- c("N","S","E","W")
+direction_list[[1]] <- c(0,1)
+direction_list[[2]] <- c(0,-1)
+direction_list[[3]] <- c(-1,0)
+direction_list[[4]] <- c(1,0)
+
+
+#store rotational directions in a dictionary list
+rotation_list <- vector(mode="list", length=2)
+names(rotation_list) <- c("R","L")
+rotation_list[[1]] <- c(-1,1)
+rotation_list[[2]] <- c(1,-1)
+
+update <- function(a,b,c,x,y,x_,y_){
+  if((a %in% names(direction_list)) && (c)){
+    x = x+ ((direction_list[[a]][1])*b)
+    y = y+ ((direction_list[[a]][2])*b)
+  }
+  
+  if((a %!in% names(direction_list)) && (!c)){
+    x_ = x_+ ((direction_list[[a]][1])*b)
+    y_ = y_+ ((direction_list[[a]][2])*b)
+  }
+  
+  if(a %in% names(rotation_list)){
+    for(i in 1:length(b%/%90)){
+      rotate <- rotation_list[[a]]
+      d <- x_
+      e <- y_
+      x_ = (e * rotate[1])
+      y_ = (d * rotate[2])
+    }
+  }
+  
+  if(a=="F"){
+    x = x + (x_*b)
+    y = y + (y_*b)
+  }
+  
+  myresults <- c(x,y,x_,y_)
+  return(myresults)
+}
+
+#-------------------------------------------------------------------------
+#Part 1: Initialization and Results
+#-------------------------------------------------------------------------
+
+x=0
+y=0
+x_=-1 #start in the east
+y_=0  #start in the east
+end_result <- update(as.character(df$directions[1]),as.numeric(df$values[1]),c=TRUE,x,y,x_,y_)
+print(end_result)
+for(j in 2:nrow(df)){
+  x=end_result[1]
+  y=end_result[2]
+  x_=end_result[3]
+  y_=end_result[4]
+  end_result <- update(as.character(df$directions[j]),as.numeric(df$values[j]),c=TRUE,x,y,x_,y_)
+  print(j,end_result)
+  }
+
+
+distance = abs(end_result[1]-0) + abs(end_result[2]-0)
+print(distance)
+
+#-------------------------------------------------------------------------
+#Part 2: Initialization and Results
+#-------------------------------------------------------------------------
