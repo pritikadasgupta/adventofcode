5c36d0b573377e2d8e9765fa73db593e703baac2
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2020-12-12 05:52:58 -0500

    updates

--- 2020/Day12/rainRisk2.py
+++ 2020/Day12/rainRisk2.py
@@ -1,37 +1,15 @@
-commands = [(line[:1], int(line[1:]))
-    for line in open("exercise12.input.txt").readlines()]
+cardinals = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
+directions = ((1, 0), (0, -1), (-1, 0), (0, 1))
+location = 0, 0
+direction = 0
 
-dirs = { 'N':(0, 1), 'S':(0, -1), 'W':(1, 0), 'E':(-1, 0) }
-rot =  { 'R':(-1, 1), 'L': (1, -1) }
+for line in open('exercise12.input.txt'):
+    action, value = line[0], int(line[1:])
 
-def execute(command, value, strafe=True):
-    global x, y, wx, wy
-    if strafe and command in dirs:  # Strafing moves the ship
-        x += dirs[command][0] * value
-        y += dirs[command][1] * value
-    if not strafe and command in dirs:  # Othervise move waypoint
-        wx += dirs[command][0] * value
-        wy += dirs[command][1] * value
-    if command in rot:
-       for i in range(value // 90):  # Rotate waypoint multiple times
-           wx, wy = wy * rot[command][0], wx * rot[command][1]
-    if command == 'F':  # Move ship in waypoint direction
-        x += wx * value
-        y += wy * value
+    if action == 'F' or action in cardinals:
+        change = directions[direction] if action == 'F' else cardinals[action]
+        location = location[0] + value * change[0], location[1] + value * change[1]
+    else:
+        direction = (direction + (value if action == 'R' else -value) // 90) % len(directions)
 
-### Part 1
-x, y   = 0, 0
-wx, wy = -1, 0  # Start by facing east
-for command, value in commands:
-    execute(command, value, strafe=True)
-distance = abs(x) + abs(y)
-print(distance)
-
-
-### Part 2
-x, y = 0, 0
-wx, wy = -10, 1  # Waypoint starts 10E 1N
-for command, value in commands:
-    execute(command, value, strafe=False)
-distance_correct = abs(x) + abs(y)
-print(distance_correct)
+print(sum(map(abs, location)))
