0c2974672e09c60d629ba8bd7c00c711eb33fa7b
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2020-12-20 20:07:53 -0500

    part 1 day 20 attempt

---
+++ 2020/Day20/jurassicJigsaw_part1.R
@@ -0,0 +1,147 @@
+#!/usr/bin/Rscript
+
+#clear workspace
+rm(list = ls())
+
+# Libraries
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+
+#------------------------------------------------------------------------------------------
+#Load Data
+#------------------------------------------------------------------------------------------
+
+#Use this if you're running from the command line:
+# mydata <- strsplit(readLines(file("stdin"))," ")
+
+#Use this if you're opening this repo as a R project, using relative paths:
+# mydata <- strsplit(readLines('2020/Day19/input_test1.txt')," ")
+mydata <- readLines('2020/Day20/input_test1.txt')
+
+#------------------------------------------------------------------------------------------
+#Functions
+#------------------------------------------------------------------------------------------
+# 90 degree clockwise rotation of matrix
+rotate <- function(x) t(apply(x, 2, rev))
+
+#------------------------------------------------------------------------------------------
+#Part 1
+#------------------------------------------------------------------------------------------
+
+tile_idx <- which(grepl("Tile", mydata, fixed = TRUE)==TRUE)
+mydata <- gsub("#", "1", mydata)
+
+#initialize
+tile_number <- 0
+tile_numbers <- vector()
+tiles <- vector("list",length(tile_idx))
+k=0
+tile_data <- vector()
+
+for(i in 1:length(mydata)){
+  if(i %in% tile_idx){
+    tile_number = as.numeric(strsplit(strsplit(mydata[i]," ")[[1]][2],":")[[1]])
+    tile_numbers <- append(tile_numbers,tile_number)
+    k=k+1
+  }else if("" %in% mydata[i]){
+    tiles[[k]] <- tile_data
+    tile_data <- vector()
+  }else{
+    curdata <- strsplit(mydata[i],"")[[1]]
+    curdata <- ifelse(curdata==".",0,1)
+    curdata <- paste(curdata,collapse="")
+    tile_data <- append(tile_data,curdata)
+  }
+}
+
+tiles[[k]] <- tile_data
+
+tiles2 <- tiles
+for(a in 1:length(tiles)){
+  mymat <- as.matrix(tiles[[a]])
+  mymat2 <- matrix(nrow = length(mymat), ncol = 10)
+  for(b in 1:nrow(mymat)){
+    replacement <- as.numeric(strsplit(mymat[b,],"")[[1]])
+    mymat2[b,] <- replacement
+  }
+  tiles2[[a]] <- mymat2
+}
+
+df_tile_numbers <- vector()
+df_tile_position <- vector()
+df_tile_position_v <- vector()
+
+for(b in 1:(length(tile_numbers))){
+  cur_tile <- tiles2[[b]]
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  top <- paste(cur_tile[1,],collapse="")
+  df_tile_position <- append(df_tile_position,"top")
+  df_tile_position_v <- append(df_tile_position_v,top)
+  
+  
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  bottom <- paste(cur_tile[10,],collapse="")
+  df_tile_position <- append(df_tile_position,"bottom")
+  df_tile_position_v <- append(df_tile_position_v,bottom)
+  
+  
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  left <- paste(cur_tile[,1],collapse="")
+  df_tile_position <- append(df_tile_position,"left")
+  df_tile_position_v <- append(df_tile_position_v,left)
+  
+
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  right <- paste(cur_tile[,10],collapse="")
+  df_tile_position <- append(df_tile_position,"right")
+  df_tile_position_v <- append(df_tile_position_v,right)
+ 
+}
+
+
+for(b in 1:(length(tile_numbers))){
+  cur_tile <- rotate(rotate(tiles2[[b]]))
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  top <- paste(cur_tile[10,],collapse="")
+  df_tile_position <- append(df_tile_position,"top_rotated")
+  df_tile_position_v <- append(df_tile_position_v,top)
+  
+  
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  bottom <- paste(cur_tile[1,],collapse="")
+  df_tile_position <- append(df_tile_position,"bottom_rotated")
+  df_tile_position_v <- append(df_tile_position_v,bottom)
+  
+  
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  left <- paste(cur_tile[,10],collapse="")
+  df_tile_position <- append(df_tile_position,"left_rotated")
+  df_tile_position_v <- append(df_tile_position_v,left)
+  
+  
+  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+  right <- paste(cur_tile[,1],collapse="")
+  df_tile_position <- append(df_tile_position,"right_rotated")
+  df_tile_position_v <- append(df_tile_position_v,right)
+  
+}
+
+
+
+my_edges_df <- as.data.frame(cbind(df_tile_numbers,df_tile_position,df_tile_position_v))
+colnames(my_edges_df) <- c("tile_id","name","value")
+my_edges_df$value <- as.character(my_edges_df$value)
+
+jigsaw1 <- inner_join(filter(my_edges_df, tile_id != first(tile_id),
+                             !grepl('rev', name)), my_edges_df, by = 'value')
+jigsaw2 <- filter(jigsaw1,tile_id.x != tile_id.y)
+jigsaw3 <- add_count(jigsaw2,tile_id.x, name = 'n.x')
+jigsaw4 <- add_count(jigsaw3,tile_id.y, name = 'n.y')
+my_jigsaw_df <- jigsaw4
+
+corner <- union(my_jigsaw_df$df_tile_numbers.x[my_jigsaw_df$n.x == 2 & my_jigsaw_df$n.y == 3],
+                my_jigsaw_df$df_tile_numbers.y[my_jigsaw_df$n.y == 2 & my_jigsaw_df$n.x == 3])
+
+print(prod(corner))
+
