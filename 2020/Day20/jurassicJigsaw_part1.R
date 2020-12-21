ad4c59580642987a8af76c073c331b741a6fb349
Pritika Dasgupta <pritika.dasgupta@gmail.com>
2020-12-20 22:26:50 -0500

    partial day 20 part 1 soln

--- 2020/Day20/jurassicJigsaw_part1.R
+++ 2020/Day20/jurassicJigsaw_part1.R
@@ -49,8 +49,8 @@ for(i in 1:length(mydata)){
     tile_data <- vector()
   }else{
     curdata <- strsplit(mydata[i],"")[[1]]
-    curdata <- ifelse(curdata==".",0,1)
-    curdata <- paste(curdata,collapse="")
+    curdata <- ifelse(curdata==".","0","1")
+    curdata <- as.character(paste(curdata,collapse=""))
     tile_data <- append(tile_data,curdata)
   }
 }
@@ -68,6 +68,7 @@ for(a in 1:length(tiles)){
   tiles2[[a]] <- mymat2
 }
 
+#NOT ROTATED
 df_tile_numbers <- vector()
 df_tile_position <- vector()
 df_tile_position_v <- vector()
@@ -99,49 +100,133 @@ for(b in 1:(length(tile_numbers))){
  
 }
 
-
-for(b in 1:(length(tile_numbers))){
-  cur_tile <- rotate(rotate(tiles2[[b]]))
-  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
-  top <- paste(cur_tile[10,],collapse="")
-  df_tile_position <- append(df_tile_position,"top_rotated")
-  df_tile_position_v <- append(df_tile_position_v,top)
-  
-  
-  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
-  bottom <- paste(cur_tile[1,],collapse="")
-  df_tile_position <- append(df_tile_position,"bottom_rotated")
-  df_tile_position_v <- append(df_tile_position_v,bottom)
-  
-  
-  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
-  left <- paste(cur_tile[,10],collapse="")
-  df_tile_position <- append(df_tile_position,"left_rotated")
-  df_tile_position_v <- append(df_tile_position_v,left)
-  
-  
-  df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
-  right <- paste(cur_tile[,1],collapse="")
-  df_tile_position <- append(df_tile_position,"right_rotated")
-  df_tile_position_v <- append(df_tile_position_v,right)
-  
-}
-
-
-
 my_edges_df <- as.data.frame(cbind(df_tile_numbers,df_tile_position,df_tile_position_v))
 colnames(my_edges_df) <- c("tile_id","name","value")
-my_edges_df$value <- as.character(my_edges_df$value)
 
-jigsaw1 <- inner_join(filter(my_edges_df, tile_id != first(tile_id),
-                             !grepl('rev', name)), my_edges_df, by = 'value')
-jigsaw2 <- filter(jigsaw1,tile_id.x != tile_id.y)
-jigsaw3 <- add_count(jigsaw2,tile_id.x, name = 'n.x')
-jigsaw4 <- add_count(jigsaw3,tile_id.y, name = 'n.y')
-my_jigsaw_df <- jigsaw4
 
-corner <- union(my_jigsaw_df$df_tile_numbers.x[my_jigsaw_df$n.x == 2 & my_jigsaw_df$n.y == 3],
-                my_jigsaw_df$df_tile_numbers.y[my_jigsaw_df$n.y == 2 & my_jigsaw_df$n.x == 3])
+# #rotated 90 degrees clockwise
+# df_tile_numbers <- vector()
+# df_tile_position <- vector()
+# df_tile_position_v <- vector()
+# 
+# for(b in 1:(length(tile_numbers))){
+#   cur_tile <- rotate(tiles2[[b]])
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   top <- paste(cur_tile[1,],collapse="")
+#   df_tile_position <- append(df_tile_position,"top_r90")
+#   df_tile_position_v <- append(df_tile_position_v,top)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   bottom <- paste(cur_tile[10,],collapse="")
+#   df_tile_position <- append(df_tile_position,"bottom_r90")
+#   df_tile_position_v <- append(df_tile_position_v,bottom)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   left <- paste(cur_tile[,1],collapse="")
+#   df_tile_position <- append(df_tile_position,"left_r90")
+#   df_tile_position_v <- append(df_tile_position_v,left)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   right <- paste(cur_tile[,10],collapse="")
+#   df_tile_position <- append(df_tile_position,"right_r90")
+#   df_tile_position_v <- append(df_tile_position_v,right)
+#   
+# }
+# 
+# my_edges_r90_df <- as.data.frame(cbind(df_tile_numbers,df_tile_position,df_tile_position_v))
+# colnames(my_edges_r90_df) <- c("tile_id","name","value")
+# 
+# #rotated 180 degrees clockwise
+# df_tile_numbers <- vector()
+# df_tile_position <- vector()
+# df_tile_position_v <- vector()
+# 
+# for(b in 1:(length(tile_numbers))){
+#   cur_tile <- rotate(rotate(tiles2[[b]]))
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   top <- paste(cur_tile[1,],collapse="")
+#   df_tile_position <- append(df_tile_position,"top_r180")
+#   df_tile_position_v <- append(df_tile_position_v,top)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   bottom <- paste(cur_tile[10,],collapse="")
+#   df_tile_position <- append(df_tile_position,"bottom_r180")
+#   df_tile_position_v <- append(df_tile_position_v,bottom)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   left <- paste(cur_tile[,1],collapse="")
+#   df_tile_position <- append(df_tile_position,"left_r180")
+#   df_tile_position_v <- append(df_tile_position_v,left)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   right <- paste(cur_tile[,10],collapse="")
+#   df_tile_position <- append(df_tile_position,"right_r180")
+#   df_tile_position_v <- append(df_tile_position_v,right)
+#   
+# }
+# 
+# my_edges_r180_df <- as.data.frame(cbind(df_tile_numbers,df_tile_position,df_tile_position_v))
+# colnames(my_edges_r180_df) <- c("tile_id","name","value")
+# 
+# 
+# 
+# #rotated 270 degrees clockwise
+# df_tile_numbers <- vector()
+# df_tile_position <- vector()
+# df_tile_position_v <- vector()
+# 
+# for(b in 1:(length(tile_numbers))){
+#   cur_tile <- rotate(rotate(rotate(tiles2[[b]])))
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   top <- paste(cur_tile[1,],collapse="")
+#   df_tile_position <- append(df_tile_position,"top_r270")
+#   df_tile_position_v <- append(df_tile_position_v,top)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   bottom <- paste(cur_tile[10,],collapse="")
+#   df_tile_position <- append(df_tile_position,"bottom_r270")
+#   df_tile_position_v <- append(df_tile_position_v,bottom)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   left <- paste(cur_tile[,1],collapse="")
+#   df_tile_position <- append(df_tile_position,"left_r270")
+#   df_tile_position_v <- append(df_tile_position_v,left)
+#   
+#   
+#   df_tile_numbers <- append(df_tile_numbers,tile_numbers[b])
+#   right <- paste(cur_tile[,10],collapse="")
+#   df_tile_position <- append(df_tile_position,"right_r270")
+#   df_tile_position_v <- append(df_tile_position_v,right)
+#   
+# }
 
-print(prod(corner))
+# my_edges_r270_df <- as.data.frame(cbind(df_tile_numbers,df_tile_position,df_tile_position_v))
+# colnames(my_edges_r270_df) <- c("tile_id","name","value")
+# 
+# my_edges_total <- merge(my_edges_df,my_edges_r90_df,by="value")
+# my_edges_total <- merge(my_edges_df,my_edges_r180_df,by="value")
+# my_edges_total <- merge(my_edges_df,my_edges_r270_df,by="value")
+
+my_edges_df_filtered <- filter(my_edges_df, tile_id != first(tile_id))
+my_edges_merged <- merge(my_edges_df_filtered,
+                              my_edges_r180_df,
+                              by = 'value')
+my_edges_merged <- filter(my_edges_merged,tile_id.x != tile_id.y) 
+my_edges_merged <- add_count(my_edges_merged,tile_id.x, name = 'n.x')
+my_edges_merged <- add_count(my_edges_merged,tile_id.y, name = 'n.y')
+
+
+corner <- union(my_edges_merged$tile_id.x[my_edges_merged$n.x == 2 & my_edges_merged$n.y == 3],
+                my_edges_merged$tile_id.y[my_edges_merged$n.y == 2 & my_edges_merged$n.x == 3])
+
+paste(prod(as.numeric(corner)))
+
 
